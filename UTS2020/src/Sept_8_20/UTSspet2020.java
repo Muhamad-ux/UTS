fc220bee10dfeaabee7bdf16d762395662543652
Admin <Admin@Namasayapermana>
2020-09-08 12:19:48 +0700

    UTS

---
+++ UTS2020/src/Sept_8_20/UTSspet2020.java
@@ -0,0 +1,13 @@
+package Sept_8_20;
+
+public class UTSspet2020 {
+	
+	public static void main (String[] args) {
+		String huruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-";
+		
+		char fn [] = {huruf.charAt(5),huruf.charAt(0),huruf.charAt(17),huruf.charAt(7),huruf.charAt(0),huruf.charAt(13),huruf.charAt(26),huruf.charAt(15),huruf.charAt(4),huruf.charAt(17),huruf.charAt(12),huruf.charAt(0),huruf.charAt(13),huruf.charAt(0 )};
+		String nama = new String(fn);
+		
+		System.out.println(nama);
+	}
+}
