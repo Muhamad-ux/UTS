d80b3582442b2a16f14f2ea179326d50a4ec39c9
Muhamad Parhan Permana <namasayapermana@gmail.com>
2020-10-27 10:47:25 +0700

    Update UTSspet2020.java

--- UTS2020/src/Sept_8_20/UTSspet2020.java
+++ UTS2020/src/Sept_8_20/UTSspet2020.java
@@ -5,9 +5,11 @@ public class UTSspet2020 {
 	public static void main (String[] args) {
 		String huruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-";
 		
-		char fn [] = {huruf.charAt(5),huruf.charAt(0),huruf.charAt(17),huruf.charAt(7),huruf.charAt(0),huruf.charAt(13),huruf.charAt(26),huruf.charAt(15),huruf.charAt(4),huruf.charAt(17),huruf.charAt(12),huruf.charAt(0),huruf.charAt(13),huruf.charAt(0 )};
-		String nama = new String(fn);
+		char fn [] = {huruf.charAt(5),huruf.charAt(0),huruf.charAt(17),huruf.charAt(7),huruf.charAt(0),huruf.charAt(13),huruf.charAt(26)};
+		char pa [] = {huruf.charAt(15),huruf.charAt(4),huruf.charAt(17),huruf.charAt(12),huruf.charAt(0),huruf.charAt(13),huruf.charAt(0)};
+		String fn = new String(fn);
+		String pa = new String(pa);
 		
-		System.out.println(nama);
+		System.out.println(fn+pa);
 	}
 }
